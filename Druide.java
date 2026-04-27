fc15ed15ca83c2b38f755c6e017eb3066b18a811
Elicopterrr <elielmendo7@gmail.com>
2026-04-27 16:20:02 +0200

    Update Druide.java

--- Druide.java
+++ Druide.java
@@ -6,7 +6,37 @@ public class Druide extends Mage{
 
   public int Gain(){
     Piece [][] jeu=getGrille();
+    int x= this.getX();
+    int y= this.getY();
+    int gain = 0;
+
+    // faut modifier si le mage est au bord
+    if(jeu[x+1][y+1].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x+1][y].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x+1][y-1].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x][y-1].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x-1][y-1].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x-1][y].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x-1][y+1].getType() == PLANTE){
+      gain++;
+    }
+    if(jeu[x][y+1].getType() == PLANTE){
+      gain++;
+    }
+
+    return gain;
     
-    if(jeu[][].getType 
-  }
+
 }

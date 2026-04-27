1601aa8e84bd4fdc40e2dd42fb5a3c8588063442
Elicopterrr <elielmendo7@gmail.com>
2026-04-27 15:55:31 +0200

    Update Druide.java

--- Druide.java
+++ Druide.java
@@ -1,3 +1,12 @@
 public class Druide extends Mage{
   
+  public Druide(int x, int y){
+    super(x,y);
+  }
+
+  public int Gain(){
+    Piece [][] jeu=getGrille();
+    
+    if(jeu[][].getType 
+  }
 }

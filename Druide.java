6b88e3e2d9415442d4ea576e80c9af17af8d161d
Elicopterrr <elielmendo7@gmail.com>
2026-05-12 13:09:53 +0200

    Update Druide.java

--- Druide.java
+++ Druide.java
@@ -10,13 +10,14 @@ public class Druide extends Mage{
     int y= this.getY();
     int gain = 0;
 
-    // faut modifier si le mage est au bord
+    // ici je parcours les cases adjacentes avec 2 boucles for 
     for(int i=-1;i<=1;i++){
       for(int j=-1;j<=1;j++){
         if(i!=0 && j!=0){
           int tempX = x+i;
           int tempY = y+j;
 
+          //je vérifie que la case actuelle est bien dans la grille 
           if(tempX >= 0 && tempX<5 && tempY <= 0 && tempY<5){
             if(jeu[tempX][tempY] instanceof Plante{
               gain++;

5ec0e96cee2a1ca06e338b14d4298def73e02d75
Elicopterrr <elielmendo7@gmail.com>
2026-05-12 13:09:07 +0200

    Update Pyromancien.java

--- Pyromancien.java
+++ Pyromancien.java
@@ -1,3 +1,30 @@
 public class Pyromancien extends Mage{
-  
+  public Pyromancien(int x, int y){
+    super(x,y);
+  }
+
+  public int Gain(){
+    Piece [][] jeu=getGrille();
+    int x= this.getX();
+    int y= this.getY();
+    int gain = 0;
+
+    // ici je parcours les cases adjacentes avec 2 boucles for 
+    for(int i=-1;i<=1;i++){
+      for(int j=-1;j<=1;j++){
+        
+        int tempX = x+i;
+        int tempY = y+j;
+
+        // je vérifie que la case actuelle est bien dans la grille 
+        if(tempX >= 0 && tempX<5 && tempY <= 0 && tempY<5){
+          if(jeu[tempX][tempY] instanceof Feu{
+            gain++;
+          }
+        }
+      }
+    }
+
+    return gain;
+  }
 }

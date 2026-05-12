2f7aa230fc65968febb8457a69e5dddcd58188b0
Elicopterrr <elielmendo7@gmail.com>
2026-05-12 13:20:06 +0200

    Update Druide.java

--- Druide.java
+++ Druide.java
@@ -4,8 +4,8 @@ public class Druide extends Mage{
     super(x,y);
   }
 
-  public int Gain(){
-    Piece [][] jeu=getGrille();
+  public int Gain(Piece [][] jeu){
+    
     int x= this.getX();
     int y= this.getY();
     int gain = 0;
@@ -13,17 +13,16 @@ public class Druide extends Mage{
     // ici je parcours les cases adjacentes avec 2 boucles for 
     for(int i=-1;i<=1;i++){
       for(int j=-1;j<=1;j++){
-        if(i!=0 && j!=0){
-          int tempX = x+i;
-          int tempY = y+j;
+  
+        int tempX = x+i;
+        int tempY = y+j;
 
-          //je vérifie que la case actuelle est bien dans la grille 
-          if(tempX >= 0 && tempX<5 && tempY >= 0 && tempY<5){
-            if(jeu[tempX][tempY] instanceof Plante{
-              gain++;
-            }
-          }         
-        }
+        //je vérifie que la case actuelle est bien dans la grille 
+        if(tempX >= 0 && tempX<5 && tempY >= 0 && tempY<5){
+          if(jeu[tempX][tempY] instanceof Plante{
+            gain++;
+          }
+        }         
       }
     }
 
